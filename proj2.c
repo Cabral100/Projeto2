6562bfc7bba29c96f25499acf386554b975cdfc9
Cabral100 <146139708+Cabral100@users.noreply.github.com>
2023-10-25 15:12:41 -0300

    Criado proj2.c com função printMenu

---
+++ proj2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "proj2.h"
+#include "string.h"
+//A funcao apenas printa as opcoes disponiveis
+void printMenu(){
+    printf("1 - Novo Cliente\n");
+    printf("2 - Deletar Cliente\n");
+    printf("3 - Listar Clientes\n");
+    printf("4 - Debito\n");
+    printf("5 - Deposito\n");
+    printf("6 - Extrato\n");
+    printf("7 - Transferencia entre contas\n");
+    printf("8 - Sair\n");
+    printf("Digite a opcao que voce quer executar: ");
+};
